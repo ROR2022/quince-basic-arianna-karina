f4a7e2300bc9c5ed4ea02ec50a9e8475c0fb90eb
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-08-19 19:37:34 -0600

    hoja1

---
+++ app/data-resume/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DataResume from '@/components/demo/DataResume'
+
+const page = () => {
+  return (
+    <div>
+      <DataResume />
+    </div>
+  )
+}
+
+export default page
---
+++ components/demo/DataResume.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { basicDemoData } from '@/components/demo/quince/basic/data/basic-demo-data';
+
+/**
+ * Este componente debera incluir todos los datos principales del evento
+ * 
+ */
+
+const DataResume = () => {
+  return (
+    <div
+    style={{
+        backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('/images/quince/encantada3.png')`,
+        filter: "brightness(0.9)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "scroll",
+        minHeight: "100vh",
+      }}
+    >
+        <div 
+        style={{minHeight: "100vh"}}
+        className='font-elegant bg-slate-300 bg-opacity-50 p-6 rounded-lg flex flex-col justify-center items-center text-center max-w-3xl mx-auto'>
+          <h2 
+        className=" font-script text-xl md:text-2xl mb-4 leading-relaxed text-white">
+          ¡Mis XV Años!
+        </h2>
+         <h2 className="font-elegant text-4xl md:text-5xl text-rose-700 mb-8">
+            {basicDemoData.event.celebrant}
+          </h2>
+        <div className="space-y-4">
+         
+          <p className="font-script text-xl md:text-2xl text-white">
+            {basicDemoData.event.date.full}
+          </p>
+          <p className="font-script text-xl md:text-2xl text-white">
+           {basicDemoData.event.ceremony.venue}
+          </p>
+          <p className="font-script text-xl md:text-2xl text-white">
+            {basicDemoData.event.ceremony.time}
+          </p>
+          <p className="font-script text-xl md:text-2xl text-white">
+            {basicDemoData.event.party.venue}
+          </p>
+            <p className="font-script text-xl md:text-2xl text-white">
+                {basicDemoData.event.party.time}
+            </p>
+        </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default DataResume
